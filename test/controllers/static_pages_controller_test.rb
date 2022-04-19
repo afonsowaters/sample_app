b9f5e120117ae2281ca6cf002d19deebfba91157
Afonso Aguas <afonso@amplemarket.com>
2022-04-19 05:54:06 +0000

    a

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,36 +3,37 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @app_title = " | Ruby on Rails Tutorial Sample App"
+    @app_title = "Ruby on Rails Tutorial Sample App"
+    @title_separator = " | "
   end
   
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home#{@app_title}"
+    assert_select "title", "#{@app_title}"
   end
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home#{@app_title}"
+    assert_select "title", "#{@app_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help#{@app_title}"
+    assert_select "title", "Help#{@title_separator}#{@app_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About#{@app_title}"
+    assert_select "title", "About#{@title_separator}#{@app_title}"
   end
   
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact#{@app_title}"
+    assert_select "title", "Contact#{@title_separator}#{@app_title}"
   end
 end
